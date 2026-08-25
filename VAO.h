4289804d7c9e59195f05842c502f70b155b16b70
boray01920 <75539373+boray01920@users.noreply.github.com>
2026-08-25 18:52:46 +0300

    Add files via upload
    pink floyd

--- VAO.h
+++ VAO.h
@@ -9,7 +9,7 @@ class VAO {
 		GLuint ID;
 		VAO();
 
-		void LinkVBO(VBO VBO, GLuint layout);
+		void LinkAttrib(VBO VBO, GLuint layout,GLuint numcomponents, GLenum Type, GLsizeiptr stride, void* offset);
 		void Bind();
 		void Unbind();
 		void Delete();
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include "shaderClass.h"
 #include "VAO.h"
@@ -27,27 +28,16 @@ int main() {
 	}
 
 	GLfloat vertices[] = {
-		0.0f, 0.7f, 0.0f, //0
-		0.4f, 0.4f, 0.0f, //1
-		0.7f, 0.4f, 0.0f, //2
-		0.6f, 0.0f, 0.0f, //3
-		0.7f, -0.6f, 0.0f, //4
-		0.0f, -0.3f, 0.0f, //5
-		-0.6f, -0.6f, 0.0f,
-		-0.6f, 0.0f, 0.0f,
-		-0.7, 0.4f, 0.0f,
-		-0.4f, 0.4f, 0.0f
+	//  COORDINATES                  COLORS
+		-0.5f, -0.5f, 0.0f,		 1.0f, 0.0f, 0.0f,	0.0f, 0.0f,	// bottom left  - red
+		 0.5f, -0.5f, 0.0f,		 0.0f, 1.0f, 0.0f,	1.0f, 0.0f,	// bottom right - green
+		 0.5f,  0.5f, 0.0f,		 0.0f, 0.0f, 1.0f,	1.0f, 1.0f,	// top right    - blue
+		-0.5f,  0.5f, 0.0f,		 1.0f, 1.0f, 0.0f,	0.0f, 1.0f  // top left     - yellow
 	};
 
 	GLuint indices[] = {
-		0,1,9,
-		1,2,3,
-		3,4,5,
-		5,6,7,
-		7,8,9,
-		1,3,5,
-		5,7,9,
-		1,9,5
+		0,1,2,
+		2,3,0
 	};
 
 	glfwMakeContextCurrent(window); // introduce the window to the current context
@@ -64,20 +54,57 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// Texture
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("animals.png", &widthImg, &heightImg, &numColCh, 4);
+	if (!bytes) {
+		std::cout << "### Failed to load texture ###" << std::endl;
+	}
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	GLenum format = (numColCh == 4) ? GL_RGBA : GL_RGB;
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
+
 
 	while (!glfwWindowShouldClose(window)) {
 
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		shaderProgram.Activate();
+		glUniform1f(uniID, 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
