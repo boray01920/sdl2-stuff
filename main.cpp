315631d42c00a681155f8799216765fbb54e5d75
boray01920 <75539373+boray01920@users.noreply.github.com>
2026-09-13 06:44:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "Texture.h"
 #include "Camera.h"
 #include "Anims.h"
+#include "MeshData.h"
 
 #define PI 3.1415926535897
 
@@ -38,109 +39,6 @@ int main() {
 		return -1;
 	}
 
-	
-
-	// Vertices coordinates
-	GLfloat vertices[] =
-	{		//     COORDINATES     /        COLORS          /    TexCoord   /        NORMALS       //
-			// Bottom face (normal: 0,-1,0)
-			-0.5f, -0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,     0.0f, -1.0f,  0.0f,
-			-0.5f, -0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 1.0f,     0.0f, -1.0f,  0.0f,
-			 0.5f, -0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 1.0f,     0.0f, -1.0f,  0.0f,
-			 0.5f, -0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 0.0f,     0.0f, -1.0f,  0.0f,
-
-			// Top face (normal: 0,1,0)
-			 -0.5f,  0.5f,  0.5f,    0.92f, 0.86f, 0.76f,    0.0f, 0.0f,     0.0f,  1.0f,  0.0f,
-			 -0.5f,  0.5f, -0.5f,    0.92f, 0.86f, 0.76f,    0.0f, 1.0f,     0.0f,  1.0f,  0.0f,
-			  0.5f,  0.5f, -0.5f,    0.92f, 0.86f, 0.76f,    1.0f, 1.0f,     0.0f,  1.0f,  0.0f,
-			  0.5f,  0.5f,  0.5f,    0.92f, 0.86f, 0.76f,    1.0f, 0.0f,     0.0f,  1.0f,  0.0f,
-
-			// Front face (normal: 0,0,1)
-			 -0.5f, -0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,     0.0f,  0.0f,  1.0f,
-			  0.5f, -0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 0.0f,     0.0f,  0.0f,  1.0f,
-			  0.5f,  0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 1.0f,     0.0f,  0.0f,  1.0f,
-			 -0.5f,  0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 1.0f,     0.0f,  0.0f,  1.0f,
-
-			// Back face (normal: 0,0,-1)
-			  0.5f, -0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,     0.0f,  0.0f, -1.0f,
-			 -0.5f, -0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 0.0f,     0.0f,  0.0f, -1.0f,
-			  -0.5f,  0.5f, -0.5f,   0.83f, 0.70f, 0.44f,    1.0f, 1.0f,     0.0f,  0.0f, -1.0f,
-			  0.5f,  0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 1.0f,     0.0f,  0.0f, -1.0f,
-
-			// Left face (normal: -1,0,0)
-			   -0.5f, -0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,    -1.0f,  0.0f,  0.0f,
-			   -0.5f, -0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 0.0f,    -1.0f,  0.0f,  0.0f,
-			   -0.5f,  0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 1.0f,    -1.0f,  0.0f,  0.0f,
-			   -0.5f,  0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 1.0f,    -1.0f,  0.0f,  0.0f,
-	
-		   // Right face (normal: 1,0,0)
-				0.5f, -0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,     1.0f,  0.0f,  0.0f,
-				0.5f, -0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 0.0f,     1.0f,  0.0f,  0.0f,
-				0.5f,  0.5f, -0.5f,    0.83f, 0.70f, 0.44f,    1.0f, 1.0f,     1.0f,  0.0f,  0.0f,
-				0.5f,  0.5f,  0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 1.0f,     1.0f,  0.0f,  0.0f,
-	};
-
-	GLuint indices[] =
-	{
-		0, 1, 2,    0, 2, 3,      // Bottom
-		4, 5, 6,    4, 6, 7,      // Top
-		8, 9, 10,   8, 10, 11,    // Front
-		12, 13, 14, 12, 14, 15,   // Back
-		16, 17, 18, 16, 18, 19,   // Left
-		20, 21, 22, 20, 22, 23    // Right
-	};
-
-	GLfloat lightVertices[] =
-	{ //     COORDINATES     /   TexCoord  //
-		// Bottom face
-		-0.1f, -0.1f,  0.1f,   0.0f, 0.0f,
-		-0.1f, -0.1f, -0.1f,   1.0f, 0.0f,
-		 0.1f, -0.1f, -0.1f,   1.0f, 1.0f,
-		 0.1f, -0.1f,  0.1f,   0.0f, 1.0f,
-
-		// Top face
-		-0.1f,  0.1f,  0.1f,   0.0f, 0.0f,
-		-0.1f,  0.1f, -0.1f,   1.0f, 0.0f,
-		 0.1f,  0.1f, -0.1f,   1.0f, 1.0f,
-		 0.1f,  0.1f,  0.1f,   0.0f, 1.0f,
-
-		// Front face
-		-0.1f, -0.1f,  0.1f,   0.0f, 0.0f,
-		 0.1f, -0.1f,  0.1f,   1.0f, 0.0f,
-		 0.1f,  0.1f,  0.1f,   1.0f, 1.0f,
-	    -0.1f,  0.1f,  0.1f,   0.0f, 1.0f,
-
-		// Back face
-		 0.1f, -0.1f, -0.1f,   0.0f, 0.0f,
-		-0.1f, -0.1f, -0.1f,   1.0f, 0.0f,
-		-0.1f,  0.1f, -0.1f,   1.0f, 1.0f,
-		 0.1f,  0.1f, -0.1f,   0.0f, 1.0f,
-
-		// Left face
-	    -0.1f, -0.1f, -0.1f,   0.0f, 0.0f,
-	    -0.1f, -0.1f,  0.1f,   1.0f, 0.0f,
-		-0.1f,  0.1f,  0.1f,   1.0f, 1.0f,
-		-0.1f,  0.1f, -0.1f,   0.0f, 1.0f,
-
-		// Right face
-		 0.1f, -0.1f,  0.1f,   0.0f, 0.0f,
-		 0.1f, -0.1f, -0.1f,   1.0f, 0.0f,
-		 0.1f,  0.1f, -0.1f,   1.0f, 1.0f,
-		 0.1f,  0.1f,  0.1f,   0.0f, 1.0f
-	};
-
-	GLuint lightIndices[] =
-	{
-		0, 1, 2,    0, 2, 3,      // Bottom
-		4, 5, 6,    4, 6, 7,      // Top
-		8, 9, 10,   8, 10, 11,    // Front
-		12, 13, 14, 12, 14, 15,   // Back
-		16, 17, 18, 16, 18, 19,   // Left
-		20, 21, 22, 20, 22, 23    // Right
-	};
-
-
-
 	glfwMakeContextCurrent(window); // introduce the window to the current context
 
 	gladLoadGL(); // load GLAD so it can configue OpenGL
@@ -149,6 +47,7 @@ int main() {
 
 	Shader shaderProgram("default.vert", "default.frag");
 
+											// main cube //
 	VAO VAO1;
 	VAO1.Bind();
 
@@ -163,7 +62,27 @@ int main() {
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	// lighting
+
+											// floor //
+	VAO floorVAO;
+	floorVAO.Bind();
+
+	VBO floorVBO(floorVertices, sizeof(floorVertices));
+	EBO floorEBO(floorIndices, sizeof(floorIndices));
+
+	floorVAO.LinkAttrib(floorVBO, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);
+	floorVAO.LinkAttrib(floorVBO, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+	floorVAO.LinkAttrib(floorVBO, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+	floorVAO.LinkAttrib(floorVBO, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));
+	floorVAO.Unbind();
+	floorVBO.Unbind();
+	floorEBO.Unbind();
+
+	glm::vec3 floorPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 floorModel = glm::mat4(1.0f);
+	floorModel = glm::translate(floorModel, floorPos);
+
+											// lighting //
 
 	Shader lightShader("light.vert", "light.frag");
 
@@ -180,6 +99,8 @@ int main() {
 	lightVBO.Unbind();
 	lightEBO.Unbind();
 
+	
+
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glm::vec3 lightPos = glm::vec3(0.5f, 1.2f, 0.5f);
@@ -191,12 +112,9 @@ int main() {
 	pyramidModel = glm::translate(pyramidModel, pyramidPos);
 
 	lightShader.Activate();
-	//glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
 	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"),lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	shaderProgram.Activate();
-	//glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-	//glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightColor.x, lightColor.y, lightColor.z);
 
 
 
@@ -206,6 +124,8 @@ int main() {
 	animals.texUnit(shaderProgram, "tex0", 0);
 	Texture evil("evil.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	evil.texUnit(lightShader, "tex0", 0);
+	Texture grass("grass.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	grass.texUnit(shaderProgram, "tex0", 0);
 
 	glEnable(GL_DEPTH_TEST);
 
@@ -214,13 +134,13 @@ int main() {
 
 	while (!glfwWindowShouldClose(window)) {
 
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClearColor(0.04f, 0.94f, 0.94f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		camera.Inputs(window);
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);		
 
-		UpdateLightAnim(lightPos, lightModel, pyramidPos, 1.0F);
+		UpdateLightAnim(lightPos, lightModel, pyramidPos, 1.5f);
 
 		shaderProgram.Activate();
 		glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
@@ -231,7 +151,12 @@ int main() {
 
 		animals.Bind();
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(floorModel));
+		grass.Bind();
+		floorVAO.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(floorIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
 		lightShader.Activate();
 		camera.Matrix(lightShader, "camMatrix");
